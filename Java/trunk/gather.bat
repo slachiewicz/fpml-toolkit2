7b5adc3f67c5aacfe687bbd5598e249051bbe14c
andrew_jacobs <andrew_jacobs@4e1ee24b-c720-42e8-9b6f-c4295506ef3a>
2012-11-30 19:02:58 +0000

    Updated gather script and 5.x validation example directory base

--- Java/trunk/gather.bat
+++ Java/trunk/gather.bat
@@ -7,7 +7,7 @@ XCOPY /S /I files-core dist\files-core
 XCOPY /S /I files-fpml dist\files-fpml
 XCOPY /S /I doc dist\doc
 CD bin
-"C:\Program Files\Java\jdk1.6.0_11\bin\jar" cvf ..\dist\handcoded.jar *
+jar cvf ..\dist\handcoded.jar *
 CD ..
 XCOPY /S /I lib dist\lib
 XCOPY /S /I manual dist\manual
