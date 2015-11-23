8cf11095b9b5dc17c8ff8d24e255e333a1b7d7fc
andrew_jacobs <andrew_jacobs@4e1ee24b-c720-42e8-9b6f-c4295506ef3a>
2015-11-23 16:52:41 +0000

    Updated scripts

--- gather-web.bat
+++ gather-web.bat
@@ -8,6 +8,8 @@ MKDIR distweb\jar\files-fpml\schemas
 MKDIR distweb\doc
 MKDIR distweb\files-fpml\examples\fpml4-7
 COPY files-core\*.* distweb\jar\files-core
+ERASE distweb\jar\files-core\releases.xml
+RENAME distweb\jar\files-core\releases-full.xml releases.xml
 COPY files-fpml\*.* distweb\jar\files-fpml
 XCOPY /S /I files-core\data distweb\jar\files-core\data
 XCOPY /S /I files-fpml\data distweb\jar\files-fpml\data
@@ -15,8 +17,9 @@ XCOPY /S /I files-fpml\schemas distweb\jar\files-fpml\schemas
 XCOPY /S /I bin\*.* distweb\jar
 XCOPY /S /I doc distweb\doc
 XCOPY /S /I files-fpml\examples\fpml4-7 distweb\files-fpml\examples\fpml4-7
+XCOPY /S /I files-fpml\examples\fpml5-9 distweb\files-fpml\examples\fpml5-9
 CD distweb\jar
-jar cvf ..\handcoded.jar *
+"C:\Program Files\Java\jdk1.8.0_31\bin\jar" cvf ..\handcoded.jar *
 CD ..\..
 RMDIR /Q /S distweb\jar
 XCOPY /S /I lib distweb\lib
