1fad91cd8f6787a7101894348cf5772aa3a4ebfc
andrew_jacobs <andrew_jacobs@4e1ee24b-c720-42e8-9b6f-c4295506ef3a>
2015-11-23 16:52:41 +0000

    Updated scripts

--- Java/trunk/gather.bat
+++ Java/trunk/gather.bat
@@ -7,7 +7,7 @@ XCOPY /S /I files-core dist\files-core
 XCOPY /S /I files-fpml dist\files-fpml
 XCOPY /S /I doc dist\doc
 CD bin
-jar cvf ..\dist\handcoded.jar *
+"C:\Program Files\Java\jdk1.8.0_31\bin\jar" cvf ..\dist\handcoded.jar *
 CD ..
 XCOPY /S /I lib dist\lib
 XCOPY /S /I manual dist\manual
